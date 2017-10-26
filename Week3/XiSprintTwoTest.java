149968efe1c273c47d3f3ce0af2df17d3d7d56bb
MindsculptorX <xzhan19@stevens.edu>
2017-10-25 22:52:27 -0400

    test part
    Test part. Run well in Main()
    Need fixed

---
+++ Week3/XiSprintTwoTest.java
@@ -0,0 +1,42 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class XiSprintTwoTest {
+	
+	@Test
+	public void testMaleLastNames() {
+//		Family fam1 = new Family();
+//		Family fam2 = new Family();
+//		
+//		fam1.setHusbandName("Daddy /Bean/");
+//		fam2.setHusbandName("Daddy /Bean/");
+//		
+//		Individual child1 = new Individual();
+//		Individual child2 = new Individual();
+//		child1.setName("Irvin /Bean/");
+//		child2.setName("Irvin /Other/");
+//		child1.setGender("M");
+//		child2.setGender("M");
+//		
+//		System.out.println(ParseGEDCOMFile.indiList.hashCode());
+//		ParseGEDCOMFile.indiList.put(10001, null);
+//		ParseGEDCOMFile.indiList.put(10002, null);
+		
+//		fam1.getChildren().add("I10001");
+//		fam2.getChildren().add("I10002");
+//		System.out.println(ReportingTool.MaleLastNames(fam1));
+//		System.out.println(ReportingTool.MaleLastNames(fam2));
+//		
+//		assertEquals(true, ReportingTool.MaleLastNames(fam1));
+//		assertEquals(false, ReportingTool.MaleLastNames(fam2));
+
+	}
+
+//	@Test
+	public void testParentsNotTooOld() {
+		Family fam1 = new Family();
+		Family fam2 = new Family();
+	}
+}
