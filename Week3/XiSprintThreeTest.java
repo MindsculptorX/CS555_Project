ac501a025680dee8f84d9e56b5f9c61eafec3447
RANE1021 <rafael.raf.nunez@gmail.com>
2017-11-16 21:49:13 -0500

    delete un-needed test

--- Week3/XiSprintThreeTest.java
+++ Week3/XiSprintThreeTest.java
@@ -40,30 +40,6 @@ public class XiSprintThreeTest {
 		fam.setChildren(new ArrayList<String>(Arrays.asList("I20001","I20002")));
 		assertEquals(false, ReportingTool3.UniqueFirstNameInFamily(fam));
 	}
-
-	
-	
-	@Test
-	public void testListDeceased(){
-		ParseGEDCOMFile.setMap();
-		ParseGEDCOMFile.indiList.clear();
-		
-		Individual indi1 = new Individual();
-		Individual indi2 = new Individual();
-		Individual indi3 = new Individual();
-		Individual indi4 = new Individual();
-		
-		indi2.setDeath("01 SEP 2000");
-		indi4.setDeath("01 SEP 2000");
-
-		ParseGEDCOMFile.indiList.put(20001, indi1);
-		ParseGEDCOMFile.indiList.put(20002, indi2);
-		ParseGEDCOMFile.indiList.put(20003, indi3);
-		ParseGEDCOMFile.indiList.put(20004, indi4);
-
-		assertEquals(Arrays.asList(indi2,indi4),ReportingTool3.ListDeceased());
-		
-	}
 	
 	//Leo's test
 	@Test
